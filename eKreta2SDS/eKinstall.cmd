82f04055a4ffb4445a6e57feb89c028ac7ca071f
martonrideg <marton.rideg@hotmail.com>
2020-03-16 09:50:54 +0100

    Update eKinstall.cmd
    Added empty directory creation for log and output

--- eKreta2SDS/eKinstall.cmd
+++ eKreta2SDS/eKinstall.cmd
@@ -4,4 +4,6 @@ echo .
 echo A telepites soran nyomj, ahol lehet "A"-t, vagy ha az nincs "Y"-t. Jo sokszor kell. 
 echo Nem csinalsz bajt vele, ha veletlenul masodszor is elinditod ezt a telepitot.
 cd %~dp0
+md log
+md output
 powershell -executionpolicy bypass ".\bin\eKinstall.ps1" 
