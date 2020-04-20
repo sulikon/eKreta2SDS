4a0aac593c6381c5428867cd8f70a99186346fbd
martonrideg <>
2020-04-21 00:20:57 +0200

    Simple tools added

---
+++ eKreta2SDS/tools/README.txt
@@ -0,0 +1,4 @@
+﻿A mappában található segédeszközöket csak mindig az adott feladatra használja.
+
+Ezek az eszközök általában nem rendelkeznek erős hibakezeléssel, 
+a kívánt célfeladatot pedig rákérdezés nélkül elvégzik.
---
+++ eKreta2SDS/tools/nev-forditas.ps1
@@ -0,0 +1,24 @@
+﻿# School Data Sync által létrehozott, fordított DisplayName-ek javítása Vezetéknév Keresztnév formára
+
+# Előfeltételek
+#
+# Rendszergazdaként futtatott PowerShellből:
+#
+# Install-Module AzureAD
+#
+#
+# Használat
+#
+# A scriptet powershellből le kell futtatni. Nem kérdez, cselekszik.
+#
+
+# bejelentkezés
+$cred = Get-Credential
+Connect-AzureAD -Credential $cred
+
+# lekérdezünk minden felhasználót
+[array]$users = Get-AzureADUser -All $true 
+
+# lecseréljük a displayname-eket "Surname Givenname" formátumúra az SDS által kezelt felhasználókon
+$users | Where-Object { $_.ExtensionProperty.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource -eq "SIS" } | ForEach-Object { Set-AzureADUser -ObjectId $_.objectid -DisplayName "$($_.surname) $($_.givenname)" }
+# örülünk.
