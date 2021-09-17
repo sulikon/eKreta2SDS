0ffa2cbcfd95310fb28fd842505e9abec55d0a02
martonrideg <marton.rideg@hotmail.com>
2021-09-17 10:43:29 +0200

    Update eKreta2SDS.cmd
    SchoolSectionSuffix

--- eKreta2SDS/eKreta2SDS.cmd
+++ eKreta2SDS/eKreta2SDS.cmd
@@ -1,50 +1,53 @@
 @echo off
-rem --- Kérem, módosítsa a megadott példaadatokat az iskola adataira! ---
+rem --- KÃ©rem, mÃ³dosÃ­tsa a megadott pÃ©ldaadatokat az iskola adataira! ---
 
-rem Az iskola oktatási azonosítója:
+rem Az iskola oktatÃ¡si azonosÃ­tÃ³ja:
 set param1=-schoolid '012345' 
 
-rem Az iskola neve. ÉKEZETET NE használjon! Ez nem látszik a felhasználóknak.
+rem Az iskola neve. Ã‰KEZETET NE hasznÃ¡ljon! Ez nem lÃ¡tszik a felhasznÃ¡lÃ³knak.
 set param2=-SchoolName 'Probavari Altalanos Iskola' 
 
-rem Az iskola címe. ÉKEZETET NE használjon! Ez nem látszik a felhasználóknak.
+rem Az iskola cÃ­me. Ã‰KEZETET NE hasznÃ¡ljon! Ez nem lÃ¡tszik a felhasznÃ¡lÃ³knak.
 set param3=-SchoolAddress '4500 Probavar Kossuth u. 26.' 
 
-rem Az Office 365 környezet domain neve
+rem Az Office 365 kÃ¶rnyezet domain neve
 set param4=-UPNSuffix 'probavarsuli.hu'
 set param5=-tenantid probavarsuli.hu
 
-rem Felhasználók kezdõ jelszavának eleje Legyen legalább 4 betû, kisbetût, nagybetût és egy jelet is tartalmazzon. 
-rem A kezdõ jelszóba az itt megadott prefix után a felhasználó saját oktatási azonosítójának utolsó 4 számjegye kerül.
+rem FelhasznÃ¡lÃ³k kezdÅ‘ jelszavÃ¡nak eleje Legyen legalÃ¡bb 4 betÅ±, kisbetÅ±t, nagybetÅ±t Ã©s egy jelet is tartalmazzon. 
+rem A kezdÅ‘ jelszÃ³ba az itt megadott prefix utÃ¡n a felhasznÃ¡lÃ³ sajÃ¡t oktatÃ¡si azonosÃ­tÃ³jÃ¡nak utolsÃ³ 4 szÃ¡mjegye kerÃ¼l.
 set param7=-PasswordPrefix 'KL.Bp' 
 
-rem --- Ez alatt a vonal alatt nem szükséges módosítani a 2020/21 tanévben ---
+rem --- Ez alatt a vonal alatt nem szÃ¼ksÃ©ges mÃ³dosÃ­tani a 2020/21 tanÃ©vben ---
 
-rem Tanév, most nem kell szerkeszteni
+rem TanÃ©v, most nem kell szerkeszteni
 set param6=-StudentYear 202021
-rem Naplózás szintje, most nem kell szerkeszteni
+rem NaplÃ³zÃ¡s szintje, most nem kell szerkeszteni
 set param8=-LogLevel "Debug"
 
-rem Vezetéknév és keresztnév fordított kezelése (Csak akkor használjuk, ha magyar nevezéktan szerint kell képezni a Displayname értéket)
-rem $true értéknél fordított nevezéktan
+rem VezetÃ©knÃ©v Ã©s keresztnÃ©v fordÃ­tott kezelÃ©se (Csak akkor hasznÃ¡ljuk, ha magyar nevezÃ©ktan szerint kell kÃ©pezni a Displayname Ã©rtÃ©ket)
+rem $true Ã©rtÃ©knÃ©l fordÃ­tott nevezÃ©ktan
 set param9=-FlipFirstnameLastname:$true
 
-rem Többiskolás környezetekben célszerû lehet az egyes iskolák csoportjainak megkülönböztetése.
-rem Itt adható meg az az elõtag, amivel minden csoport neve kezdõdik - ha meg van adva.
-rem set param10=-SchoolSectionPrefix "Próbavár"
+rem TÃ¶bbiskolÃ¡s kÃ¶rnyezetekben cÃ©lszerÅ± lehet az egyes iskolÃ¡k csoportjainak megkÃ¼lÃ¶nbÃ¶ztetÃ©se.
+rem Itt adhatÃ³ meg az az elÅ‘tag, amivel minden csoport neve kezdÅ‘dik - ha meg van adva.
+rem set param10=-SchoolSectionPrefix "PrÃ³bavÃ¡r"
 
+rem TÃ¶bbiskolÃ¡s kÃ¶rnyezetekben cÃ©lszerÅ± lehet az egyes iskolÃ¡k csoportjainak megkÃ¼lÃ¶nbÃ¶ztetÃ©se.
+rem Itt adhatÃ³ meg az az utÃ³tag, amely minden csoport nevÃ©be bekerÃ¼l a tanÃ©v elÃ© - ha meg van adva.
+rem set param11=-SchoolSectionSuffix "PrÃ³bavÃ¡r"
 
-rem Windows Credential Manager-ben létrehozott Credential neve, ha nincs megadva, akkor minden furásnál be kell jelentkezni
-rem set param11=-AzureADCredential "eKreta2SDS-"
+rem Windows Credential Manager-ben lÃ©trehozott Credential neve, ha nincs megadva, akkor minden furÃ¡snÃ¡l be kell jelentkezni
+rem set param12=-AzureADCredential "eKreta2SDS-"
 
-rem AzureAD-ban regisztrált alkalmazás azonosítója GRAPH API-hoz. Ha nincs megadva, akkor nem lesz Graph Api használva.
-rem set param12=-AppId "123456"
+rem AzureAD-ban regisztrÃ¡lt alkalmazÃ¡s azonosÃ­tÃ³ja GRAPH API-hoz. Ha nincs megadva, akkor nem lesz Graph Api hasznÃ¡lva.
+rem set param13=-AppId "123456"
 
-rem APP Kulcs GRAPH API-hoz. Ha nincs megadva, akkor nem lesz Graph Api használva.
-rem set param13=-AppSecret "123456"
+rem APP Kulcs GRAPH API-hoz. Ha nincs megadva, akkor nem lesz Graph Api hasznÃ¡lva.
+rem set param14=-AppSecret "123456"
 
 echo Ugye nem felejtetted el tanulmanyozni az UTMUTATO.txt-t?
 echo .
 cd %~dp0
-powershell -executionpolicy bypass ".\bin\eKretaLaunch.ps1" %param1% %param2% %param3% %param4% %param5% %param6% %param7% %param8% %param9% %param10% %param11% %param12% %param13%
+powershell -executionpolicy bypass ".\bin\eKretaLaunch.ps1" %param1% %param2% %param3% %param4% %param5% %param6% %param7% %param8% %param9% %param10% %param11% %param12% %param13% %param14%
 pause Ellenorizd a kimenetet, masold ki a hibakat, ha voltak! Aztan nyomj meg egy gombot. Reszletes naplok a TraceLog mappaban vannak.
